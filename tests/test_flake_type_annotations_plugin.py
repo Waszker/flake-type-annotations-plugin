fe2a8bec0b8f9ed4d705479a44db9878424b4561
Piotr Waszkiewicz <waszka23@gmail.com>
2022-08-01 01:19:52 +0200

    Tests

--- tests/test_flake_type_annotations_plugin.py
+++ tests/test_flake_type_annotations_plugin.py
@@ -11,3 +11,16 @@ def test_flake_process_runs_correctly() -> None:
     stdout, _ = process.communicate()
 
     assert len(stdout) == 0
+
+
+def test_plugin_properly_detects_errors() -> None:
+    """Confirms that errors are properly returned from flake."""
+    process = subprocess.Popen(
+        ["flake8", "tests/fixtures/invalid_flake_file.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout, _ = process.communicate()
+
+    assert b"tests/fixtures/invalid_flake_file.py:7:5: ANN001:" in stdout
+    assert b"tests/fixtures/invalid_flake_file.py:11:21: ANN002:" in stdout
