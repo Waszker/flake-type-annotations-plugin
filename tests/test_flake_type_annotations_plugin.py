07bd9007a99f10cae4092fd33b01c00dd21f456f
Piotr Waszkiewicz <waszka23@gmail.com>
2022-10-03 13:26:34 +0200

    Test line numbers.

--- tests/test_flake_type_annotations_plugin.py
+++ tests/test_flake_type_annotations_plugin.py
@@ -31,7 +31,7 @@ def test_plugin_properly_detects_errors() -> None:
     assert b"tests/fixtures/invalid_flake_file.py:7:26: TAN001:" in stdout
     assert b"tests/fixtures/invalid_flake_file.py:8:21: TAN002:" in stdout
     assert b"tests/fixtures/invalid_flake_file.py:8:26: TAN001:" in stdout
-    assert b"tests/fixtures/invalid_flake_file.py:18:25: TAN001:" in stdout
-    assert b"tests/fixtures/invalid_flake_file.py:18:43: TAN001:" in stdout
+    assert b"tests/fixtures/invalid_flake_file.py:17:25: TAN001:" in stdout
+    assert b"tests/fixtures/invalid_flake_file.py:17:43: TAN001:" in stdout
     assert b"tests/fixtures/invalid_flake_file.py:22:26: TAN002:" in stdout
     assert b"tests/fixtures/invalid_flake_file.py:22:41: TAN002:" in stdout
